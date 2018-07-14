8fc157fe4a65fdbbe64fef962aebb71d6b41bf9a
邢腾飞 <xingtengfei@xingtengfeideMacBook-Air.local>
2018-07-14 09:48:54 +0800

    上传工程

---
+++ XTF/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XTF
+//
+//  Created by 邢腾飞 on 2018/7/14.
+//  Copyright © 2018年 邢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XTF/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XTF
+//
+//  Created by 邢腾飞 on 2018/7/14.
+//  Copyright © 2018年 邢腾飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
